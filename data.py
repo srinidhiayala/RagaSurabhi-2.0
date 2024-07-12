41748bc88fadfeba252fe3987ca89a892f86d79d
srinidhiayala <145507362+srinidhiayala@users.noreply.github.com>
2024-07-12 16:47:44 -0400

    all but carnaticFile setup

--- data.py
+++ data.py
@@ -40,6 +40,66 @@ def isParent(soup):
                     break
                 
             return [False," ".join(parentName), melaNumber]
+        
+def getRagaDetailsString(soup):
+    div_tag = soup.find("div", class_="body_assetpage_details_box")
+    
+    if div_tag:
+        full_text = div_tag.get_text(separator=' ', strip=True)
+        pretty = ' '.join(full_text.split())
+    
+    return pretty
+
+def getAroAva(string):
+    
+    if "Note" in string:
+        return -1
+    index = string.rfind("Arohanam")
+    whole = string[:index]
+    aro = whole[9:whole.find("Avarohanam")]. strip()
+    ava = whole[whole.find("Avarohanam")+11:].strip()
+    
+    return [aro,ava]
+    
+def aroAvaFile(soup):
+    # div_tag = soup.find("div", class_="body_assetpage_details_box")
+    ul_tag = soup.find("ul", class_="playlist")
+    
+    if ul_tag:
+        first_li = ul_tag.find('li')
+        
+        if "Arohanam" in first_li.get_text():
+       
+            aroavalink = first_li.find('a')
+            if aroavalink:
+                link_url = aroavalink['href']
+            
+        else:
+            return
+            
+    return ("https://www.ragasurabhi.com"+link_url)
+    
+def signatureFile(soup):
+    
+    ul_tag = soup.find("ul", class_="playlist")
+    
+    if ul_tag:
+        li_tags = ul_tag.find_all('li')
+        if len(li_tags) > 1:
+            second_li = li_tags[1]
+            
+    if second_li:    
+        if "Signature" in second_li.get_text():
+            a_tag = second_li.find('a')
+            if a_tag:
+                link_url = a_tag['href']
+        else:
+            return 
+    
+    return ("https://www.ragasurabhi.com"+link_url)
+    
+
+
 
 
 url = "https://www.ragasurabhi.com/carnatic-music/ragas.html"
@@ -64,9 +124,38 @@ if response.status_code == 200:
             html_content2 = new_response.text
             soup2 = BeautifulSoup(html_content2, 'html.parser')
             # determines if ragam is parent or not
-            typeOfRagam=isParent(soup2) 
-            print(typeOfRagam)
-            # handle if -1 CASE!!!!
+            isRagaParent = isParent(soup2)
+            
+            if isRagaParent != -1:
+                if isRagaParent[0] == True:
+                    # ragam is parent, get mela num
+                    melaNumber = isRagaParent[1]
+                elif isRagaParent[0] == False:
+                    # ragam is not parent, get parent and mela number
+                    parentName = isRagaParent[1]
+                    melaNumber = isRagaParent[2]
+
+            
+            # get all details String for raga
+            details = getRagaDetailsString(soup2)
+            # get Aro Ava
+            aroAvaDetails = getAroAva(details) 
+            if aroAvaDetails != -1:
+                aro, ava = aroAvaDetails[0], aroAvaDetails[1]
+            
+            # generates the Aro/Ava link
+            aroAvaLink = aroAvaFile(soup2)
+            print(aroAvaLink)
+            
+            # generates the signature link
+            signatureLink = signatureFile(soup2)
+            
+            # generates carnatic song and file
+            
+                
+            
+            
+            
         
 else:
     print(f"Failed to retrieve the page: {response.status_code}")
--- ragam.py
+++ ragam.py
@@ -1,18 +1,20 @@
 class Ragam:
-    def __init__(self, name, aro, ava, aroAvaFile):
+    def __init__(self, name, aro, ava, aroAvaFile, signatureFile, carnaticFile):
         self.name = name  
         self.aro=aro
         self.ava=ava
         self.aroAvaFile=aroAvaFile
+        self.signatureFile = signatureFile
+        self.carnaticFile = carnaticFile
 
 
 class Parent(Ragam):
-    def __init__(self, name, aro, ava, aroAvaFile, melaNumber):
-        super().__init__(name, aro, ava, aroAvaFile)
+    def __init__(self, name, aro, ava, aroAvaFile, signatureFile, carnaticFile, melaNumber):
+        super().__init__(name, aro, ava, aroAvaFile, signatureFile, carnaticFile)
         self.melaNumber = melaNumber
 
 class Child(Ragam):
-    def __init__(self, name, aro, ava, aroAvaFile, parent, melaNumber):
-        super().__init__(name, aro, ava, aroAvaFile)  
+    def __init__(self, name, aro, ava, aroAvaFile, signatureFile, carnaticFile, parent, melaNumber):
+        super().__init__(name, aro, ava, aroAvaFile,signatureFile, carnaticFile)  
         self.parent = parent
         self.melaNumber = melaNumber
